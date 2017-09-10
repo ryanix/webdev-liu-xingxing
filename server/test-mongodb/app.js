ae4067e7122c8e26e5f4d4f576ad46111ebee1a7
Xingxing liu <Ryanix@Xingxings-MacBook-Pro.local>
2017-09-10 09:23:17 -0400

    build before deploy

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -18,7 +18,7 @@ module.exports = function(app)
   }
 
   var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
+    mongoose.connect(connectionString,{useMongoClient:true});
 
     var TestSchema = mongoose.Schema({
         message: String
