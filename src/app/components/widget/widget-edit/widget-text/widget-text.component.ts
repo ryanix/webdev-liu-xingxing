82ace7cdb1228b0ec02a43ff098f65cb5ea951ce
Xingxing liu <Ryanix@Xingxings-MacBook-Pro.local>
2017-11-06 15:30:20 -0500

    finish new widget implementation revise read me

--- src/app/components/widget/widget-edit/widget-text/widget-text.component.ts
+++ src/app/components/widget/widget-edit/widget-text/widget-text.component.ts
@@ -1,16 +1,16 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {Widget} from '../../../../models/widget.model.client';
-import {ActivatedRoute, Router} from '@angular/router';
-import {WidgetServiceClient} from '../../../../services/widget.service.client';
-import {NgForm} from '@angular/forms';
+import {Widget} from "../../../../models/widget.model.client";
+import {NgForm} from "@angular/forms";
+import {ActivatedRoute, Route, Router} from "@angular/router";
+import {WidgetServiceClient} from "../../../../services/widget.service.client";
 
 @Component({
-  selector: 'app-widget-html',
-  templateUrl: './widget-html.component.html',
-  styleUrls: ['./widget-html.component.css']
+  selector: 'app-widget-text',
+  templateUrl: './widget-text.component.html',
+  styleUrls: ['./widget-text.component.css']
 })
-export class WidgetHtmlComponent implements OnInit {
-  @ViewChild('f') youtubeForm: NgForm;
+export class WidgetTextComponent implements OnInit {
+  @ViewChild('f') textForm: NgForm;
 
   userId: String;
   webId: String;
@@ -18,16 +18,16 @@ export class WidgetHtmlComponent implements OnInit {
   widgetId: String;
   widget: Widget;
   errorFlag: boolean;
-  errorMsg = 'HTML name should not be empty';
+  errorMsg = 'Placeholder is required for text input widget.';
 
   constructor(
     private route: ActivatedRoute,
-    private widgetService: WidgetServiceClient,
-    private router: Router,
+    private  widgetService: WidgetServiceClient,
+    private router: Router
   ) { }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe( params => {
       this.userId = params['uid'];
       this.webId = params['wid'];
       this.pageId = params['pid'];
@@ -36,19 +36,21 @@ export class WidgetHtmlComponent implements OnInit {
         .subscribe((w: Widget) => {
           this.widget = w;
         });
+
     });
   }
 
   confirmChange() {
-    if (this.youtubeForm.valid) {
+    if (this.textForm.valid) {
+      this.errorFlag = false;
       this.widgetService.updateWidget(this.widgetId, this.widget)
         .subscribe((w: Widget) => {
           if (w) {
             this.router.navigate([`/user/${this.userId}/website/${this.webId}/page/${this.pageId}/widget`]);
-          } else {
-            this.errorFlag = true;
-          }
-        });
+        }
+      });
+    }else {
+      this.errorFlag = true;
     }
   }
 
